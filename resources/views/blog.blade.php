7fd70bf0bc6c19b8612cc55e16f1db47fb3c5fc2
Revprm <revy.pramana@gmail.com>
2024-09-18 10:29:51 +0700

    feat: added 2 articles in blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,24 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot>
-        <h3 class="text-xl">Welcome to My Blog!</h3>
+        <article class="py-8 max-w-screen-md border-b border-gray-300">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+            <div class="text-base text-gray-500">
+                <a href="#">Revy Pramana</a> | 18 September 2024
+            </div>
+            <p class="my-4 font-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sunt blanditiis optio aut fugit a hic ipsum modi cum esse quos cupiditate quae quo accusantium quasi laudantium, dolor eum!
+            </p>
+            <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+        </article>
+
+        <article class="py-8 max-w-screen-md border-b border-gray-300">
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+            <div class="text-base text-gray-500">
+                <a href="#">Revy Pramana</a> | 15 September 2024
+            </div>
+            <p class="my-4 font-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sunt blanditiis optio aut fugit a hic ipsum modi cum esse quos cupiditate quae quo accusantium quasi laudantium, dolor eum!
+            </p>
+            <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+        </article>
 </x-layout>
