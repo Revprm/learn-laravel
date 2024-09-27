dc7e18088d8210daaeca491b12bc0e7e9e14197f
Revprm <revy.pramana@gmail.com>
2024-09-27 13:47:45 +0700

    feat: dynamic data using Eloquent ORM with Database SQLite

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Revy Pramana',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sunt blanditiis optio aut fugit a hic ipsum modi cum esse quos cupiditate quae quo accusantium quasi laudantium, dolor eum!'
-
-            ],
-            [
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Revy Pramana',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vel sunt blanditiis optio aut fugit a hic ipsum modi cum esse quos cupiditate quae quo accusantium quasi laudantium, dolor eum!'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+    use HasFactory;
 }
