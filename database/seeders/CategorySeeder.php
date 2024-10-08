aba6dc2aedd0a59a1a9228a5d4c2833054d67c42
Revprm <revy.pramana@gmail.com>
2024-10-08 10:40:30 +0700

    feat: added Database Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+        Category::create([
+            'name' => 'UI UX',
+            'slug' => 'ui-ux',
+        ]);
+        Category::create([
+            'name' => 'Machine Learning',
+            'slug' => 'machine-learning',
+        ]);
+        Category::create([
+            'name' => 'Data Structure',
+            'slug' => 'data-structure',
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ratione ipsum quas quia ea debitis deserunt dicta, nisi quasi perspiciatis voluptatem est voluptas aspernatur quidem sunt aliquid! Est, officiis at.'
+        // ]);
+    
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
