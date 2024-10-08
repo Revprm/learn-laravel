76fa0e47d5b320a4ec02895db2b2e30b32a7fdda
Revprm <revy.pramana@gmail.com>
2024-10-08 10:09:58 +0700

    feat: Post Category

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 use App\Models\Post;
-use App\Models\User;
+use App\Models\User;    
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home page']);
@@ -25,8 +26,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
-    return view('posts', ['title' => 'Article by ' . $user->name, 'post' => $user->posts]);
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
